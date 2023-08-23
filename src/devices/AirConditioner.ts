243d0eaf4749e8dc51e150383b6d949a2e0ae352
dalbbang <49666778+dalbbang@users.noreply.github.com>
2023-08-23 17:08:04 +0900

    Update AirConditioner.ts

--- src/devices/AirConditioner.ts
+++ src/devices/AirConditioner.ts
@@ -426,7 +426,7 @@ export default class AirConditioner extends baseDevice {
 
     this.platform.log.info('Set fan speed = ', speedValue);
     const device: Device = this.accessory.context.device;
-    const windStrength = parseInt(Object.keys(FanSpeed)[speedValue - 1]) || FanSpeed.HIGH;
+    const windStrength = parseInt(Object.keys(FanSpeed)[speedValue - 1]) || FanSpeed.AUTO;
     this.platform.ThinQ?.deviceControl(device.id, {
       dataKey: 'airState.windStrength',
       dataValue: windStrength,
